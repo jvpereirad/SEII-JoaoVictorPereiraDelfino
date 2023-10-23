0aa054d57a931af358e784139ec91c89d2a3dfe1
jvpereirad <42525235+jvpereirad@users.noreply.github.com>
2023-10-23 16:30:38 -0300

    Create py02.py

---
+++ Semana02/ex-03/py02.py
@@ -0,0 +1,10 @@
+# Recebendo entrada do usuário
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+# Convertendo a entrada de idade para um número inteiro
+idade = int(idade)
+
+# Exibindo os valores inseridos pelo usuário
+print("Nome:", nome)
+print("Idade:", idade)
