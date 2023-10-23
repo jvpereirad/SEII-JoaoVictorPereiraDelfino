00a3fb41df7bf8c87a105425cc952546f85ad3a5
jvpereirad <42525235+jvpereirad@users.noreply.github.com>
2023-10-23 16:31:19 -0300

    Create py03.py

---
+++ Semana02/ex-03/py03.py
@@ -0,0 +1,8 @@
+# Conversão de tipos
+numero_inteiro = 42
+numero_decimal = float(numero_inteiro)
+numero_texto = str(numero_inteiro)
+
+print("Número inteiro:", numero_inteiro)
+print("Número decimal:", numero_decimal)
+print("Número como texto:", numero_texto)
