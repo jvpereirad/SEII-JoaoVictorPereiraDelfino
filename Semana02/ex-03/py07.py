3c36969c1adbde015841d0f4408b846b8e45aa4f
jvpereirad <42525235+jvpereirad@users.noreply.github.com>
2023-10-23 16:38:33 -0300

    Create py07.py

---
+++ Semana02/ex-03/py07.py
@@ -0,0 +1,17 @@
+# Operadores de comparação
+a = 10
+b = 5
+
+igual = a == b
+diferente = a != b
+maior = a > b
+menor = a < b
+maior_ou_igual = a >= b
+menor_ou_igual = a <= b
+
+print("Igual:", igual)
+print("Diferente:", diferente)
+print("Maior:", maior)
+print("Menor:", menor)
+print("Maior ou Igual:", maior_ou_igual)
+print("Menor ou Igual:", menor_ou_igual)
