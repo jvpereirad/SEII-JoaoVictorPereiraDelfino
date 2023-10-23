79e40256df2f92db431788873fd7c1b7771f8270
jvpereirad <42525235+jvpereirad@users.noreply.github.com>
2023-10-23 16:32:26 -0300

    Create py05.py

---
+++ Semana02/ex-03/py05.py
@@ -0,0 +1,15 @@
+# Operadores aritméticos
+a = 10
+b = 5
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto_divisao = a % b
+
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+print("Resto da divisão:", resto_divisao)
