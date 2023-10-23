b5f6be6bc5aac0bdfaf9edbfe1f0e3c1c9b12456
jvpereirad <42525235+jvpereirad@users.noreply.github.com>
2023-10-23 16:32:47 -0300

    Create py06.py

---
+++ Semana02/ex-03/py06.py
@@ -0,0 +1,6 @@
+# Precedência de operadores
+resultado = 3 + 4 * 2  # Multiplicação tem precedência sobre adição
+outra_resultado = (3 + 4) * 2  # Parênteses podem ser usados para alterar a precedência
+
+print("Resultado:", resultado)
+print("Outro resultado:", outra_resultado)
