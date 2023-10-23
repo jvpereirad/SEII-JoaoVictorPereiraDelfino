50799138e09e99e90aca2118e854a9130f099e73
jvpereirad <42525235+jvpereirad@users.noreply.github.com>
2023-10-23 16:32:01 -0300

    Create py04.py

---
+++ Semana02/ex-03/py04.py
@@ -0,0 +1,16 @@
+# Manipulação de strings
+nome = "Joao"
+sobrenome = "Delfino"
+
+# Concatenação de strings
+nome_completo = nome + " " + sobrenome
+
+# Tamanho da string
+tamanho = len(nome_completo)
+
+# Acesso a caracteres individuais
+primeira_letra = nome_completo[0]
+
+print("Nome completo:", nome_completo)
+print("Tamanho:", tamanho)
+print("Primeira letra:", primeira_letra)
